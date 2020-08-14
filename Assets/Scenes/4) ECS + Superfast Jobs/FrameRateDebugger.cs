e7f44a78bb723176210604150ef88d2cacf553d9
BadGraphixD <badgraphixcontact@gmail.com>
2020-08-15 00:45:43 +0200

    Additional scene and frame debugger

---
+++ Assets/Scenes/4) ECS + Superfast Jobs/FrameRateDebugger.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class FrameRateDebugger : MonoBehaviour {
+
+    [SerializeField] private Text text;
+    [SerializeField] private float refreshTime = 0.5f;
+
+    int frameCounter = 0;
+    float timeCounter = 0.0f;
+    float lastFramerate = 0.0f;
+
+    private void Update() {
+        if( timeCounter < refreshTime ) {
+            timeCounter += Time.deltaTime;
+            frameCounter++;
+        }
+        else {
+            lastFramerate = (float)frameCounter / timeCounter;
+            frameCounter = 0;
+            timeCounter = 0.0f;
+            
+            text.text = "FPS: " + Mathf.FloorToInt(lastFramerate).ToString();
+        }
+    }
+}
